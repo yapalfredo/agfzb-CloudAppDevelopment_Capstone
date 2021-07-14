6d448b6d9763bd805d83f0f60dec5bfbd4895e30
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-07-14 10:46:17 -0400

    finished week 1 of Capstone project

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
@@ -10,9 +11,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","alyap111-oc.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
